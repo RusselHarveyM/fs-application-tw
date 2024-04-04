315ab71ab9c73d7bbb888d83bc6a988350005d6c
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-04-04 11:10:54 +0800

    revert from useReducer implementation

--- src/data/data-context.tsx
+++ src/data/data-context.tsx
@@ -1,4 +1,4 @@
-import { createContext, useEffect, useReducer, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import axios from "axios";
 
 export const DataContext = createContext({
@@ -10,93 +10,61 @@ export const DataContext = createContext({
   ratings: undefined,
   comments: undefined,
   redTags: undefined,
-  updateEntry: () => {},
+  addUser: () => {},
+  addBuilding: () => {},
+  addRoom: () => {},
+  addSpace: () => {},
+  addSpaceImage: () => {},
+  addRating: () => {},
+  addComment: () => {},
+  addRedTag: () => {},
 });
 
-async function fetchAllData() {
-  try {
-    const newData = {
-      users: [],
-      buildings: [],
-      rooms: [],
-      spaces: [],
-    };
-    newData.users = (
-      await axios.get(
-        "https://fs-backend-copy-production.up.railway.app/api/user"
-      )
-    ).data;
-    newData.buildings = (
-      await axios.get(
-        "https://fs-backend-copy-production.up.railway.app/api/buildings"
-      )
-    ).data;
-    newData.rooms = (
-      await axios.get(
-        "https://fs-backend-copy-production.up.railway.app/api/rooms"
-      )
-    ).data;
-    newData.spaces = (
-      await axios.get(
-        "https://fs-backend-copy-production.up.railway.app/api/space"
-      )
-    ).data;
-    console.log(newData);
-    return newData;
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-export function dataReducer(state, action) {
-  if (action.type === "ADD_ENTRY") {
-    //..
-  }
-  if (action.type === "UPDATE_ENTRY") {
-    //..
-  }
-  if (action.type === "DELETE_ENTRY") {
-    //..
-  }
-  if (action.type === "READ_ENTRIES") {
-    const data = fetchAllData();
-    return { data };
-  }
-
-  return state;
-}
-
 export default function DataContextProvider({ children }) {
-  const [data, dataDispatch] = useReducer(dataReducer, {
+  const [data, setData] = useState({
     users: undefined,
     buildings: undefined,
     rooms: undefined,
     spaces: undefined,
   });
 
-  /*
-  handleUpdateEntry
-    action - object
-    {
-        type: ADD_ENTRY/UPDATE_ENTRY/..
-        endpoint: https://..
-        data: {} // if necessary.
+  async function fetchAllData() {
+    try {
+      const newData = {
+        users: [],
+        buildings: [],
+        rooms: [],
+        spaces: [],
+      };
+      newData.users = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/user"
+        )
+      ).data;
+      newData.buildings = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/buildings"
+        )
+      ).data;
+      newData.rooms = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/rooms"
+        )
+      ).data;
+      newData.spaces = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/space"
+        )
+      ).data;
+      console.log(newData);
+      setData(newData);
+    } catch (error) {
+      console.log(error);
     }
-  */
-  function handleUpdateEntry(action) {
-    dataDispatch({
-      type: action.type,
-      payload: {
-        endpoint: action.endpoint,
-        data: action.data ?? undefined,
-      },
-    });
   }
 
   useEffect(() => {
-    dataDispatch({
-      type: "READ_ENTRIES",
-    });
+    fetchAllData();
   }, []);
 
   const dataCtx = {
@@ -104,7 +72,6 @@ export default function DataContextProvider({ children }) {
     buildings: data.buildings,
     rooms: data.rooms,
     spaces: data.spaces,
-    updateEntry: handleUpdateEntry,
   };
   return (
     <DataContext.Provider value={dataCtx}>{children}</DataContext.Provider>
