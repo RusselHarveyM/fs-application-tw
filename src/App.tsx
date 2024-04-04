b48f2539671308e480c399ab75708dd003ba1a26
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-04-04 10:49:59 +0800

    implement useReducer hook in dataContext

--- src/App.tsx
+++ src/App.tsx
@@ -5,63 +5,20 @@ import ManageContent from "./components/ManageContent";
 import SettingsContent from "./components/SettingsContent";
 import Popup from "./components/Popup";
 
-import axios from "axios";
-import { DataContext } from "./data/data-context";
+import DataContextProvider from "./data/data-context";
 
 function App() {
   const [contentDisplay, setContentDisplay] = useState("dashboard");
   const [isPopup, setIsPopup] = useState(true);
-  const [data, setData] = useState({
-    users: undefined,
-    buildings: undefined,
-    rooms: undefined,
-    spaces: undefined,
-  });
 
   function handleContentDisplay(selected: SetStateAction<string>) {
     setContentDisplay(selected);
-  }
-
-  async function fetchAllData() {
-    try {
-      const newData = {
-        users: [],
-        buildings: [],
-        rooms: [],
-        spaces: [],
-      };
-      newData.users = (
-        await axios.get(
-          "https://fs-backend-copy-production.up.railway.app/api/user"
-        )
-      ).data;
-      newData.buildings = (
-        await axios.get(
-          "https://fs-backend-copy-production.up.railway.app/api/buildings"
-        )
-      ).data;
-      newData.rooms = (
-        await axios.get(
-          "https://fs-backend-copy-production.up.railway.app/api/rooms"
-        )
-      ).data;
-      newData.spaces = (
-        await axios.get(
-          "https://fs-backend-copy-production.up.railway.app/api/space"
-        )
-      ).data;
-      console.log(newData);
-      setData(newData);
-    } catch (error) {
-      console.log(error);
-    }
   }
 
   useEffect(() => {
     const timer = setTimeout(() => {
       setIsPopup(false);
     }, 5000);
-    fetchAllData();
     return () => clearTimeout(timer);
   }, []);
 
@@ -69,13 +26,6 @@ function App() {
 
   if (contentDisplay === "manage") content = <ManageContent />;
   else if (contentDisplay === "settings") content = <SettingsContent />;
-
-  const dataCtx = {
-    users: data.users,
-    buildings: data.buildings,
-    rooms: data.rooms,
-    spaces: data.spaces,
-  };
 
   return (
     <main className="flex flex-col m-auto">
@@ -94,7 +44,7 @@ function App() {
           </p>
         </Popup>
       )}
-      <DataContext.Provider value={dataCtx}>{content}</DataContext.Provider>
+      <DataContextProvider>{content}</DataContextProvider>
     </main>
   );
 }
