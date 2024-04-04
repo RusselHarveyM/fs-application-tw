3fb90828325434ddc67f887a8d870c70480e7f86
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-04-04 09:00:11 +0800

    outsource data context state&managements

--- src/data/data-context.tsx
+++ src/data/data-context.tsx
@@ -1,4 +1,5 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
 
 export const DataContext = createContext({
   users: undefined,
@@ -18,3 +19,61 @@ export const DataContext = createContext({
   addComment: () => {},
   addRedTag: () => {},
 });
+
+export default function DataContextProvider({ children }) {
+  const [data, setData] = useState({
+    users: undefined,
+    buildings: undefined,
+    rooms: undefined,
+    spaces: undefined,
+  });
+
+  async function fetchAllData() {
+    try {
+      const newData = {
+        users: [],
+        buildings: [],
+        rooms: [],
+        spaces: [],
+      };
+      newData.users = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/user"
+        )
+      ).data;
+      newData.buildings = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/buildings"
+        )
+      ).data;
+      newData.rooms = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/rooms"
+        )
+      ).data;
+      newData.spaces = (
+        await axios.get(
+          "https://fs-backend-copy-production.up.railway.app/api/space"
+        )
+      ).data;
+      console.log(newData);
+      setData(newData);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchAllData();
+  }, []);
+
+  const dataCtx = {
+    users: data.users,
+    buildings: data.buildings,
+    rooms: data.rooms,
+    spaces: data.spaces,
+  };
+  return (
+    <DataContext.Provider value={dataCtx}>{children}</DataContext.Provider>
+  );
+}
