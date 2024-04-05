2afedc289eedb9cfe1d695094a80f92fd4947517
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-04-05 19:24:22 +0800

    make image gallery comp leaner

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -1,7 +1,7 @@
 export default function ImageGallery({ images }) {
   return (
     <div className="grid grid-cols-4 grid-rows-5 w-1/3 h-[30rem] bg-neutral-100 rounded-lg">
-      {images.length > 0 ? (
+      {images && images?.length > 0 ? (
         images.map((image) => (
           <img
             key={image.id}
@@ -11,8 +11,9 @@ export default function ImageGallery({ images }) {
           />
         ))
       ) : (
-        <p className="text-neutral-500 text-lg w-fit mx-auto my-52">
-          No Images yet...
+        <p className="text-neutral-500 text-lg w-40 h-fit relative top-52 left-32">
+          No Images yet
+          <p className="animate-bounce text-2xl w-fit h-fit">...</p>
         </p>
       )}
     </div>
