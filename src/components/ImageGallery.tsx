09504e51b5c02be932d78ce4a997b677ebc717cc
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-04-06 07:33:28 +0800

    remove round style in imagegallery

--- src/components/ImageGallery.tsx
+++ src/components/ImageGallery.tsx
@@ -3,30 +3,19 @@ export default function ImageGallery({ images, onSelectImage }) {
 
   return (
     <div
-      className={`grid grid-cols-4 grid-rows-5 w-1/3 h-[30rem] rounded-lg bg-neutral-100 ${
+      className={`grid grid-cols-4 grid-rows-5 w-1/3 h-[30rem] bg-neutral-100 ${
         images && images?.length > 0 ? undefined : cssLoad
       }`}
     >
       {images && images?.length > 0 ? (
-        images.map((image: any, index: number) => {
-          let css = "";
-          if (index === 0) {
-            css = "rounded-tl-lg";
-          } else if (index === 3) {
-            css = "rounded-tr-lg";
-          } else if (index === 19) {
-            css = "rounded-bl-lg";
-          } else if (index === 23) {
-            css = "rounded-br-lg";
-          }
-
+        images.map((image: any) => {
           return (
             <img
               key={image.id}
               src={`data:image/jpeg;base64,${image.image}`}
               alt="gallery-image"
               onClick={() => onSelectImage(image.image)}
-              className={`h-full w-full object-fit rounded-bl  hover:scale-105 hover:cursor-pointer ${css}`}
+              className={`h-full w-full object-fit  hover:scale-105 hover:cursor-pointer`}
             />
           );
         })
