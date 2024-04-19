c2a53b3c10b9667f0e9c1b1248a673cf593a0d58
Hareram Chua <chuahareram@gmail.com>
2024-04-19 21:20:19 +0800

    Remove add entry in column

--- src/payments/columns/RoomColumns.tsx
+++ src/payments/columns/RoomColumns.tsx
@@ -92,47 +92,6 @@ export const roomColumns: ColumnDef<Room>[] = [
     },
   },
   {
-    header: ({ column }) => {
-      const addRoomModal = useRef();
-      const { useEntry } = useContext(DataContext);
-
-      async function handleAddRoom(roomData) {
-        try {
-          const roomDataWithId = { ...roomData, Id: "string" }; // Include id with a placeholder value
-          const action = {
-              type: "rooms",
-              method: "post",
-              data: roomDataWithId,
-          };
-          // Call the useEntry function to add a new user
-          console.log(roomDataWithId);
-          useEntry(action);
-          console.log(`Room added successfully`);
-          addRoomModal.current.close(); // Close the modal after successful addition
-        } catch (error) {
-          console.error("Error adding room:", error);
-        }
-      }
-
-      return (
-        <>
-          <AddRoomModal
-            ref={addRoomModal}
-            onSubmit={handleAddRoom}
-            buttonCaption="Add Entry"
-            buttonVariant="red"
-          />
-          <Button
-            variant="ghost"
-            className="flex items-center"
-            onClick={() => addRoomModal.current.open()}
-          >
-            Add Entry
-            <Plus className="h-4 w-4" />
-          </Button>
-        </>
-      );
-    },
     id: "actions",
     cell: ({ row }) => {
       const room = row.original;
