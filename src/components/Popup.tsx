421a6f26fb4ab4e6fdbc7d8268671824b319e1f4
Muxxano <russelmercadosacs@gmail.com>
2024-04-29 18:29:27 +0800

    fix popup layout

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -2,7 +2,7 @@ import { createPortal } from "react-dom";
 
 export default function Popup({ children }) {
   return createPortal(
-    <div className="md:w-1/3 sm:w-2/3 bg-white absolute top-28 left-1/3 text-xs md:text-base py-2 px-12 text-center shadow rounded-full m-auto hover:shadow-md hover:cursor-pointer animate-slide-in-top">
+    <div className="md:w-1/3 mx-auto sm:w-2/5 bg-white absolute top-28 left-1/3 text-xs md:text-base py-2 px-12 text-center shadow rounded-full m-auto hover:shadow-md hover:cursor-pointer animate-slide-in-top">
       {children}
     </div>,
     document.getElementById("modal-root") as Element
