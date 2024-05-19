4574c28d9163c46f950c8783ebfacd5ea3f84337
RusselHarveyM <russelharvey.mercado@cit.edu>
2024-05-19 19:40:24 +0800

    modify popup text size

--- src/components/Popup.tsx
+++ src/components/Popup.tsx
@@ -3,7 +3,7 @@ import { createPortal } from "react-dom";
 export default function Popup({ children }) {
   return createPortal(
     <div
-      className="md:w-1/3 mx-auto sm:w-2/5 bg-white absolute top-20 sm:top-28 left-1/2 transform -translate-x-1/2 text-xs md:text-base py-2 px-12 text-center shadow rounded-full m-auto hover:shadow-md hover:cursor-pointer animate-slide-in-top"
+      className="md:w-1/3 xs:w-[20rem] mx-auto sm:w-2/5 bg-white absolute top-20 sm:top-28 left-1/2 transform -translate-x-1/2 text-xs md:text-base xs:text-xs py-2 px-12 text-center shadow rounded-full m-auto hover:shadow-md hover:cursor-pointer animate-slide-in-top"
       style={{ zIndex: 999 }}
     >
       {children}
